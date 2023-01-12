2c0500337ee8e1e934b0d1ea816204f4416cdaf9
wendy munyasi <wendymunyasi@gmail.com>
2023-01-12 22:49:01 +0300

    Add task 4 - Buy buy buy

---
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -0,0 +1,14 @@
+-- Write a SQL script that creates a trigger that decreases the quantity
+-- of an item after adding a new order.
+-- Quantity in the table items can be negative.
+DROP TRIGGER IF EXISTS reduce_quantity;
+DELIMITER $$
+CREATE TRIGGER reduce_quantity
+AFTER INSERT ON orders
+FOR EACH ROW
+BEGIN
+    UPDATE items
+        SET quantity = quantity - NEW.number
+        WHERE name = NEW.item_name;
+END $$
+DELIMITER ;
