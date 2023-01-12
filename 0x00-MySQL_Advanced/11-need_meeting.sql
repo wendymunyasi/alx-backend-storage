c0672e67d6eaf93f91ced872bb191b2de148686b
wendy munyasi <wendymunyasi@gmail.com>
2023-01-12 23:13:03 +0300

    Add task 11 -  No table for a meeting

---
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -0,0 +1,18 @@
+-- Write a SQL script that creates a view need_meeting that lists all
+-- students that have a score under 80 (strict) and no last_meeting or
+-- more than 1 month.
+
+-- Requirements:
+-- The view need_meeting should return all students name when:
+-- They score are under (strict) to 80
+-- AND no last_meeting date OR more than a month
+DROP VIEW IF EXISTS need_meeting;
+CREATE VIEW need_meeting AS
+    SELECT name
+        FROM students
+        WHERE score < 80 AND
+            (
+                last_meeting IS NULL
+                OR last_meeting < SUBDATE(CURRENT_DATE(), INTERVAL 1 MONTH)
+            )
+;
