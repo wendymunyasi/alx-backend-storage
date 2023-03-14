fecae2f38b4462984feb4a6a7b3e2c6e80d623f9
wendy munyasi <wendymunyasi@gmail.com>
2023-03-14 08:02:58 +0300

    Commit message: Update cache decorator to cache func result and count requests

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 from functools import wraps
-import time
 from typing import Callable
 import requests
 import redis
@@ -8,7 +7,7 @@ import redis
 redis_client = redis.Redis()
 
 
-def cache(expirration: int) -> Callable:
+def cache(expirration: Callable) -> Callable:
     """A decorator that caches the result of a function with a Redis backend.
 
     Args:
@@ -18,49 +17,33 @@ def cache(expirration: int) -> Callable:
         Callable: A decorated function that caches its result.
     """
     @wraps(expirration)
-    def wrapper(func: callable) -> Callable:
+    def wrapper(func: callable) -> str:
         """The actual caching decorator.
 
         Args:
             func (callable): The function to cache.
 
         Returns:
-            Callable:  A wrapper function that caches the result of func.
+            str:  A wrapper function that caches the result of func.
         """
-        def inner(url: str) -> str:
-            """The wrapper function that caches the result of func.
-
-            Args:
-                url (str): The URL to retrieve.
-
-            Returns:
-                str: The cached HTML content of the URL, if available;
-                otherwise, the result of func.
-            """
-            cache_key = "cache:{}".format(url)
-            # Check if the result is cached and not expired
-            if redis_client.exists(cache_key) and time.time() \
-                    - float(redis_client.hget(cache_key,
-                                              'timestamp')) < expirration:
-                # Cache hit: return the cached content
-                print("Cache hit for URL: {}".format(url))
-                return redis_client.hget(cache_key, 'content').decode('utf-8')
-            else:
-                # Cache miss: retrieve the result from the function & cache it
-                print("Cache miss for URL: {}".format(url))
-                # call the original function to get the result
-                content = func(url)
-                # cache the result
-                redis_client.hmset(
-                    cache_key, {'timestamp': time.time(), 'content': content})
-                # set the expiration time
-                redis_client.expire(cache_key, expirration)
-                return content
-        return inner
+        # increment the count of how many times this func has been requested
+        redis_client.incr('count:{}'.format(func))
+        # get the cached result for this func
+        result = redis_client.get('result:{}'.format(func))
+        # if there is a cached result, return it
+        if result:
+            return result.decode('utf-8')
+        # if there is no cached result, fetch the data and cache it
+        result = expirration(func)
+        redis_client.set('count:{}'.format(func), 0)
+        # set the result to expire after 10 seconds
+        redis_client.setex('result:{}'.format(func), 10, result)
+        return result
+    # return the wrapped function
     return wrapper
 
 
-@cache(expiration=10)
+@cache()
 def get_page(url: str) -> str:
     """Retrieves the HTML content of a URL.
 
