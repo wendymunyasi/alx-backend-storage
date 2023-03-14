8d75b32c0f1d49c6f1a9971c39cb7dd7bced3c42
wendy munyasi <wendymunyasi@gmail.com>
2023-03-14 15:59:55 +0300

    Update `wrapped` function in `cache` decorator to implement an expiring web cache and tracker

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,97 +1,44 @@
 #!/usr/bin/env python3
 """Module for implementing an expiring web cache and tracker
 """
-from functools import wraps
-from typing import Callable
 import requests
-import redis
+import time
+from functools import wraps
 
-redis_client = redis.Redis()
+CACHE_EXPIRATION_TIME = 10  # seconds
+CACHE = {}
 
 
-def cache(expiration: int) -> Callable:
+def cache(fn):
     """_summary_
 
     Args:
-        expiration (int): _description_
+        fn (function): _description_
 
     Returns:
-        Callable: _description_
+        _type_: _description_
     """
-    def decorator(func: Callable) -> Callable:
+    @wraps(fn)
+    def wrapped(*args, **kwargs):
         """_summary_
 
-        Args:
-            func (Callable): _description_
-
         Returns:
-            Callable: _description_
+            _type_: _description_
         """
-        @wraps(func)
-        def wrapper(*args, **kwargs) -> str:
-            """_summary_
-
-            Returns:
-                str: _description_
-            """
-            key = f"{func.__name__}:{args}:{kwargs}"
-            redis_client.incr(f"count:{key}")
-            result = redis_client.get(f"result:{key}")
-            if result:
-                return result.decode("utf-8")
-            result = func(*args, **kwargs)
-            redis_client.setex(f"result:{key}", expiration, result)
-            return result
-        return wrapper
-    return decorator
-
-
-def expiration(time: int) -> Callable[[Callable], Callable]:
-    """_summary_
-
-    Args:
-        time (int): _description_
-
-    Returns:
-        Callable[[Callable], Callable]: _description_
-    """
-    def decorator(func: Callable) -> Callable:
-        """_summary_
-
-        Args:
-            func (Callable): _description_
-
-        Returns:
-            Callable: _description_
-        """
-        @wraps(func)
-        def wrapper(*args, **kwargs):
-            """sumary_line
-
-            Keyword arguments:
-            argument -- description
-            Return: return_description
-            """
-
-            # Generate a key based on the function name and arguments
-            key = f'{func.__name__}:{args}:{kwargs}'
-            # Check if the result is already cached
-            result = redis_client.get(key)
-            if result is not None:
-                # If it is, return the cached result
-                return result.decode('utf-8')
-            else:
-                # If it isn't, execute the function and cache the result with
-                # expiration time
-                result = func(*args, **kwargs)
-                redis_client.setex(key, time, result)
-                return result
-        return wrapper
-    return decorator
+        url = args[0]
+        if url in CACHE and CACHE[url]["timestamp"] + CACHE_EXPIRATION_TIME > \
+                time.time():
+            CACHE[url]["count"] += 1
+            return CACHE[url]["content"]
+        else:
+            content = fn(*args, **kwargs)
+            CACHE[url] = {"content": content,
+                          "timestamp": time.time(), "count": 1}
+            return content
+    return wrapped
 
 
 @cache
-@expiration(10)
 def get_page(url: str) -> str:
     """_summary_
 
@@ -101,4 +48,5 @@ def get_page(url: str) -> str:
     Returns:
         str: _description_
     """
-    return requests.get(url).text
+    response = requests.get(url)
+    return response.content.decode('utf-8')
