9965984672555a1c9a0e4dab8e6891e132e3d5db
wendy munyasi <wendymunyasi@gmail.com>
2023-03-14 15:38:50 +0300

    if __name__ == "__main__":     print(get_page("https://www.holbertonschool.com"))Update caching logic and add expiration time for cached results in `get_page` function

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -8,18 +8,37 @@ import redis
 
 redis_client = redis.Redis()
 
-# Define a new function to handle expiration time for cached results
-
-
-def expiration(time: int) -> Callable[[Callable], Callable]:
+def cache(func: Callable) -> Callable:
     """_summary_
 
     Args:
-        time (int): _description_
+        func (Callable): _description_
 
     Returns:
-        Callable[[Callable], Callable]: _description_
+        Callable: _description_
     """
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        """_summary_
+
+        Returns:
+            _type_: _description_
+        """
+        # Generate a key based on the function name and arguments
+        key = f'{func.__name__}:{args}:{kwargs}'
+        # Check if the result is already cached
+        result = redis_client.get(key)
+        if result is not None:
+            # If it is, return the cached result
+            return result.decode('utf-8')
+        else:
+            # If it isn't, execute the function and cache the result
+            result = func(*args, **kwargs)
+            redis_client.set(key, result)
+            return result
+    return wrapper
+
+def expiration(time: int) -> Callable[[Callable], Callable]:
     def decorator(func: Callable) -> Callable:
         """_summary_
 
@@ -29,14 +48,15 @@ def expiration(time: int) -> Callable[[Callable], Callable]:
         Returns:
             Callable: _description_
         """
-        # Define a new wrapper function to apply the caching logic
         @wraps(func)
         def wrapper(*args, **kwargs):
-            """_summary_
-
-            Returns:
-                _type_: _description_
+            """sumary_line
+            
+            Keyword arguments:
+            argument -- description
+            Return: return_description
             """
+                        
             # Generate a key based on the function name and arguments
             key = f'{func.__name__}:{args}:{kwargs}'
             # Check if the result is already cached
@@ -45,25 +65,23 @@ def expiration(time: int) -> Callable[[Callable], Callable]:
                 # If it is, return the cached result
                 return result.decode('utf-8')
             else:
-                # If it isn't, execute the function and cache the result
+                # If it isn't, execute the function and cache the result with expiration time
                 result = func(*args, **kwargs)
                 redis_client.setex(key, time, result)
                 return result
         return wrapper
     return decorator
 
-# Apply the cache decorator to the get_page function with a 10 second
-# expiration time
-
-
+@cache
 @expiration(10)
 def get_page(url: str) -> str:
-    """Retrieves the HTML content of a URL.
+    """_summary_
 
     Args:
-        url (str): The URL to retrieve.
+        url (str): _description_
 
     Returns:
-        str: The HTML content of the URL.
+        str: _description_
     """
-    return requests.get(url).text
+    response = requests.get(url)
+    return response.text
